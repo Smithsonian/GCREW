cd328e61d0df81f49be801edd74e2dd30732c6dd
taylorarlette <167914369+taylorarlette@users.noreply.github.com>
2025-11-24 11:11:55 -0500

    Update and rename render_daily_check.R to daily_checks/render_daily_check.R

--- daily_checks/render_daily_check.R
+++ daily_checks/render_daily_check.R
@@ -3,4 +3,5 @@
 Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files (x86)/pandoc-3.7.0.1")
 setwd("C:/Users/LaGorgaL.S/Documents/Chapata_Stem/daily_checks")
 library(rmarkdown)
+
 render("C:/Users/LaGorgaL.S/Documents/Chapata_Stem/daily_checks/chapada_checks.Rmd")
