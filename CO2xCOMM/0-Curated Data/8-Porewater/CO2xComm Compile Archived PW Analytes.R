c926030ebea15c0ffff878770de44dbc97afe4f9
Victoria Ellis <EllisV@si.edu>
2026-06-08 14:52:39 -0400

    Add files via upload

---
+++ CO2xCOMM/0-Curated Data/8-Porewater/CO2xComm Compile Archived PW Analytes.R
@@ -0,0 +1,319 @@
+# Compile All Archived Porewater Analyte Data By Experiment
+# By: Erin Fien (05.04.2023)
+# Edited by Zoe Read (01/26/2024)
+
+##Clear Environment
+rm(list=ls()) 
+
+##Read in packages
+library(dplyr)
+
+# Set Working Directory --------------------------------------------------------
+# General Porewater Archive Working Directory
+wd <- "S:/Biogeochemistry/GCREW/1-CO2xCommunity Experiment/0-Curated Data/8-Porewater"
+
+
+# Import All Analyte Data ------------------------------------------------------
+# Manually Edit File Names with Year and Publication Dates
+
+# 1-Sulfide 
+setwd(paste0(wd,"/1-Sulfide", sep=""))
+sulfide <- read.csv("CO2xComm Master Porewater Sulfide 2025 (published 01-26-2026).csv")
+sulfide$Day_sulfide <- sulfide$Day
+sulfide$Day <- NULL
+sulfide <- sulfide %>%
+  mutate(Month = factor(Month, levels = month.name)) 
+sulfide <- sulfide[with(sulfide, order(Year, Month, Chamber, Depth)),]
+sulfide <- sulfide %>% 
+  group_by(Year, Month, SampleID) %>% 
+  dplyr::mutate(dupe = n()>1)
+sulfide_dups <- subset(sulfide, dupe == "TRUE")
+sulfide$Treatment <- NULL
+
+# 2-pH
+setwd(paste0(wd,"/2-pH", sep=""))
+pH <- read.csv("CO2xComm Master Porewater pH 2025 (published 01-26-2026).csv")
+pH$Day_pH <- pH$Day
+pH$Day <- NULL
+pH <- pH %>%
+  mutate(Month = factor(Month, levels = month.name)) 
+pH <- pH[with(pH, order(Year, Month, Chamber, Depth)),]
+pH <- pH %>% 
+  group_by(Year, Month, SampleID) %>% 
+  dplyr::mutate(dupe = n()>1)
+pH_dups <- subset(pH, dupe == "TRUE")
+pH$Treatment <- NULL
+
+# 3-CH4
+setwd(paste0(wd,"/3-CH4", sep=""))
+CH4 <- read.csv("CO2xComm Master Porewater Methane 2025 (published 01-26-2026).csv")
+CH4$Day_CH4 <- CH4$Day
+CH4$Day <- NULL
+CH4 <- CH4 %>%
+  mutate(Month = factor(Month, levels = month.name)) 
+CH4 <- CH4[with(CH4, order(Year, Month, Chamber, Depth)),]
+CH4 <- CH4 %>% 
+  group_by(Year, Month, SampleID) %>% 
+  dplyr::mutate(dupe = n()>1)
+CH4_dups <- subset(CH4, dupe == "TRUE")
+CH4$Treatment <- NULL
+
+# 4-NH4
+setwd(paste0(wd,"/4-NH4", sep=""))
+NH4 <- read.csv("CO2xComm Master Porewater NH4 2025 (published 01-26-2026).csv")
+NH4$X <- NULL
+NH4$SampleID <- toupper(NH4$SampleID)
+NH4 <- NH4 %>%
+  mutate(Month = factor(Month, levels = month.name)) 
+NH4 <- NH4[with(NH4, order(Year, Month, Chamber, Depth)),]
+NH4 <- NH4 %>% 
+  group_by(Year, Month, SampleID) %>% 
+  dplyr::mutate(dupe = n()>1)
+NH4_dups <- subset(NH4, dupe == "TRUE")
+NH4$Treatment <- NULL
+
+# 5-Sulfate & Chloride
+setwd(paste0(wd,"/5-Sulfate & Chloride", sep=""))
+SO4.Cl <- read.csv("CO2xComm Master Porewater SO4 & Cl 2025 (published 01-26-2026).csv")
+SO4.Cl$Day_SO4.Cl <- SO4.Cl$Day
+SO4.Cl$Day <- NULL
+SO4.Cl <- SO4.Cl %>%
+  mutate(Month = factor(Month, levels = month.name)) 
+SO4.Cl <- SO4.Cl[with(SO4.Cl, order(Year, Month, Chamber, Depth)),]
+SO4.Cl <- SO4.Cl %>% 
+  group_by(Year, Month, SampleID) %>% 
+  dplyr::mutate(dupe = n()>1)
+SO4.Cl_dups <- subset(SO4.Cl, dupe == "TRUE")
+SO4.Cl$Treatment <- NULL
+
+
+##Change either SO4 or Cl from -99 to 0 if the other one is present. B/c Dionex gives value na instead of 0 when values is below detection limit, even though the sample was run. 
+SO4.Cl$SO4 <- as.numeric(SO4.Cl$SO4)
+SO4.Cl$Cl <- as.numeric(SO4.Cl$Cl)
+
+SO4.Cl$SO4 <- ifelse(SO4.Cl$Cl > 0 & is.na(SO4.Cl$SO4) | SO4.Cl$Cl > 0 & SO4.Cl$SO4 == -99, 0, SO4.Cl$SO4)
+SO4.Cl$Cl <- ifelse(SO4.Cl$SO4 > 0 & is.na(SO4.Cl$Cl) | SO4.Cl$SO4 > 0 & SO4.Cl$Cl == -99, 0, SO4.Cl$Cl)
+
+##Change negative values to zero
+SO4.Cl$SO4 <- ifelse(SO4.Cl$SO4 < 0 & SO4.Cl$SO4 > -99, 0, SO4.Cl$SO4)
+SO4.Cl$Cl <- ifelse(SO4.Cl$Cl < 0 & SO4.Cl$Cl > -99, 0, SO4.Cl$Cl)
+
+
+# Merge Anolytes ---------------------------------------------------------------
+# Add pH 
+all <- full_join(sulfide, pH, by=c('Year', 'Month', 'Community', 'Chamber', 'Depth', 'SampleID'))
+
+# Add CH4 
+all <- full_join(all, CH4, by=c('Year', 'Month', 'Community', 'Chamber', 'Depth', 'SampleID'))
+
+# Add NH4
+all <- full_join(all, NH4, by=c('Year', 'Month', 'Community', 'Chamber', 'Depth', 'SampleID'))
+
+# Add SO4 and Cl
+all <- full_join(all, SO4.Cl, by=c('Year', 'Month', 'Community', 'Chamber', 'Depth', 'SampleID'))
+
+
+##Add in Treatment Information 
+setwd(paste0(wd,"/0-All Porewater Data", sep=""))
+Trt <- read.csv("CO2xComm Sample List.csv")
+
+all <- full_join(all, Trt, by=c('SampleID'))
+
+
+##Keep only the lowest (sample) date
+all$Sample_Date <- apply(all[,c("Day_sulfide", "Day_pH", "Day_CH4")], 1, min, na.rm = TRUE) 
+
+
+
+
+##Fix certain sample dates 
+
+#September 30 2003 to October 1 2003 (Sc)
+
+all <- within(all, {
+  f <- Month == 'September' & Sample_Date == '30' & Year == '2003' & Community == 'SC'
+  Month[f] <- 'October'
+  Sample_Date[f] <- '1'
+  Year[f] <- '2003'
+}) 
+
+#September 30 2003 to October 2 2003 (Mx)
+
+all <- within(all, {
+  f <- Month == 'September' & Sample_Date == '30' & Year == '2003' & Community == 'MX'
+  Month[f] <- 'October'
+  Sample_Date[f] <- '2'
+  Year[f] <- '2003'
+}) 
+
+#April 5 2005 to May 5 2005 (Sc)
+
+all <- within(all, {
+  f <- Month == 'April' & Sample_Date == '5' & Year == '2005' & Community == 'SC'
+  Month[f] <- 'May'
+  Sample_Date[f] <- '5'
+  Year[f] <- '2005'
+}) 
+
+#October 31 2004 to November 1 2004 (Mx)
+all <- within(all, {
+  f <- Month == 'October' & Sample_Date == '31' & Year == '2004' & Community == 'MX'
+  Month[f] <- 'November'
+  Sample_Date[f] <- '1'
+  Year[f] <- '2004'
+}) 
+
+
+#November 30 2004 to December 2 2004 (Sp, Mx)
+all <- within(all, {
+  f <- Month == 'November' & Sample_Date == '30' & Year == '2004' & Community == 'SP'
+  Month[f] <- 'December'
+  Sample_Date[f] <- '2'
+  Year[f] <- '2004'
+}) 
+
+all <- within(all, {
+  f <- Month == 'November' & Sample_Date == '30' & Year == '2004' & Community == 'MX'
+  Month[f] <- 'December'
+  Sample_Date[f] <- '2'
+  Year[f] <- '2004'
+}) 
+
+#December 31 2004 to January 3 2005 (Sc) 
+all <- within(all, {
+  f <- Month == 'December' & Sample_Date == '31' & Year == '2004' & Community == 'SC'
+  Month[f] <- 'January'
+  Sample_Date[f] <- '3'
+  Year[f] <- '2005'
+}) 
+
+#December 31 2004 to January 4 2005 (Sp, Mx) 
+all <- within(all, {
+  f <- Month == 'December' & Sample_Date == '31' & Year == '2004' & Community == 'SP'
+  Month[f] <- 'January'
+  Sample_Date[f] <- '4'
+  Year[f] <- '2005'
+}) 
+
+all <- within(all, {
+  f <- Month == 'December' & Sample_Date == '31' & Year == '2004' & Community == 'MX'
+  Month[f] <- 'January'
+  Sample_Date[f] <- '4'
+  Year[f] <- '2005'
+}) 
+
+
+
+##Replace infinity sample dates with NA 
+all[sapply(all, is.infinite)] <- NA
+all[all=="Inf"] <- NA
+
+##Make all -99 values NA 
+all[all==-99] <- NA
+
+##Replace all other negative values with 0 
+all[all < 0] <- 0
+
+##Remove other Dates 
+all$Day_sulfide <- NULL
+all$Day_pH <- NULL 
+all$Day_CH4 <- NULL
+all$dupe <- NULL
+all$dupe.x <- NULL
+all$dupe.x.x <- NULL
+all$dupe.y <- NULL
+all$dupe.y.y <- NULL
+
+
+##Replace NA values with -99 
+all$Sample_Date <- as.numeric(all$Sample_Date)
+all$H2S[is.na(all$H2S)] <- -99
+all$pH[is.na(all$pH)] <- -99
+all$CH4[is.na(all$CH4)] <- -99
+all$NH4[is.na(all$NH4)] <- -99
+all$SO4[is.na(all$SO4)] <- -99
+all$Cl[is.na(all$Cl)] <- -99 
+all$Salinity[is.na(all$Salinity)] <- -99
+
+
+
+##Sort by year, month, chamber, and depth 
+all <- all %>%
+  mutate(Month = factor(Month, levels = month.name)) 
+
+all$Community <- as.factor(all$Community)
+all$Community <- factor(all$Community, levels=c('SC', 'SP', 'MX'))
+
+
+all <- all[with(all, order(Year, Month, Community, Chamber, Depth)),]
+
+all
+
+all <- subset(all, select = c(Year, Month, Sample_Date, Community, Chamber, Treatment, Depth, SampleID, 
+                              H2S, pH, CH4, NH4, SO4, Cl, Salinity))
+
+
+# Export Data ------------------------------------------------------------------
+setwd(paste0(wd,"/0-All Porewater Data", sep=""))
+write.csv(all, "CO2xComm Master Porewater All Data 2002-2025 (published 01-27-2026).csv", row.names = F)
+
+
+
+# ##Calculate percentage of missing data in each column for each Month/Year 
+# 
+# ##Make all -99 values NA 
+# all[all==-99] <- NA
+# 
+# 
+# all
+# ##Group by Month/Year and calculate %NA values
+# NA_H2S <- all %>%
+#   group_by(Year, Month) %>%
+#   summarise(H2S_NA = 100 * sum(is.na(H2S)/length(H2S)))
+# 
+# NA_pH <- all %>%
+#   group_by(Year, Month) %>%
+#   summarise(pH_NA = 100 * sum(is.na(pH)/length(pH)))
+# 
+# NA_CH4 <- all %>%
+#   group_by(Year, Month) %>%
+#   summarise(CH4_NA = 100 * sum(is.na(CH4)/length(CH4)))
+# 
+# NA_NH4 <- all %>%
+#   group_by(Year, Month) %>%
+#   summarise(NH4_NA = 100 * sum(is.na(NH4)/length(NH4)))
+# 
+# NA_SO4 <- all %>%
+#   group_by(Year, Month) %>%
+#   summarise(SO4_NA = 100 * sum(is.na(SO4)/length(SO4)))
+# 
+# NA_Cl <- all %>%
+#   group_by(Year, Month) %>%
+#   summarise(Cl_NA = 100 * sum(is.na(Cl)/length(Cl)))
+# 
+# NA_Salinity <- all %>%
+#   group_by(Year, Month) %>%
+#   summarise(Salinity_NA = 100 * sum(is.na(Salinity)/length(Salinity)))
+# 
+# NA_all <- full_join(NA_H2S, NA_pH, by=c('Year','Month'))
+# NA_all <- full_join(NA_all, NA_CH4, by=c('Year','Month'))
+# NA_all <- full_join(NA_all, NA_NH4, by=c('Year','Month'))
+# NA_all <- full_join(NA_all, NA_SO4, by=c('Year','Month'))
+# NA_all <- full_join(NA_all, NA_Cl, by=c('Year','Month'))
+# NA_all <- full_join(NA_all, NA_Salinity, by=c('Year','Month'))
+# 
+# NA_all <- NA_all %>% 
+#   mutate_if(is.numeric, round)
+# NA_all
+# 
+# NA_100 <- subset(NA_all, H2S_NA == 100 | pH_NA == 100 | CH4_NA == 100 | NH4_NA == 100 | SO4_NA == 100 | Cl_NA == 100 | Salinity_NA== 100)
+# 
+# keep <- apply(NA_100[3:9], 1, function(x) length(unique(x[!is.na(x)])) != 1)
+# NA_100_select <- NA_100[keep, ]
+# NA_100_select[, 3:9][NA_100_select[3:9]!=100] <- NA
+# 
+# # Export Percent Missing Data
+# setwd(paste0(wd,"/0-All Porewater Data", sep=""))
+# write.csv(NA_all, "CO2xComm Percent Missing Data 2002-2023.csv", row.names = F)
+# write.csv(NA_100_select, "CO2xComm Months and Years with All Data Missing 2002-2023.csv", row.names = F)
+
