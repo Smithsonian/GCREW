f0a2ca5f09df941f8ccc88fd660915f4120d0ea0
taylorarlette <167914369+taylorarlette@users.noreply.github.com>
2026-06-22 16:55:03 -0400

    add WaterLevel processing to GCREW repo.

---
+++ WaterLevel/data_processing_scripts/01_WaterLevel_to_CSV.R
@@ -0,0 +1,56 @@
+#### Step 1 -- Convert raw loggernet file to a CSV. break the data into monthly chunks #### 
+#Description -- 
+#The raw data from loggernet are .dat files
+#Need to be .csv files to manipulate
+
+#### Load Functions and Directories ####
+invisible(lapply(list.files("functions/", pattern = "\\.R$", full.names = TRUE), source))
+
+# previous relevant directories
+#rawData_dir <- paste0(Sys.getenv("dropbox_filepath") , "GCREW_LOGGERNET_DATA/0_RawData/unprocessed_data/")
+#rawDataArchive_dir <- paste0(Sys.getenv("dropbox_filepath") , "GCREW_LOGGERNET_DATA/0_RawData/archive_data/")
+#rawCSVData_dir <- paste0(Sys.getenv("dropbox_filepath") , "GCREW_LOGGERNET_WORKFLOW/1_RawCSVData/unprocessed/")
+
+#current relevant directories
+#will have to modify once dropbox folder is created
+rawData_dir <- paste0(Sys.getenv("TE_dropbox_filepath") , "Taylor_Projects/TEST/0_RawData/unprocessed_archive_data/")
+rawDataArchive_dir <- paste0(Sys.getenv("TE_dropbox_filepath") , "Taylor_Projects/TEST/0_RawData/processed_archive_data/")
+rawCSVData_dir <- paste0(Sys.getenv("TE_dropbox_filepath") , "Taylor_Projects/TEST/1_RawCSVData/unprocessed/")
+
+#### Step 1  -- Convert to CSV ####
+#Description -- Load the raw Loggernet files and save them as .csv files Then archive the raw Loggernet file. 
+
+#list files in the raw data folder to be converted. 
+files <- list.files(rawData_dir, full.names = T, ignore.case = T)%>%
+  str_subset(pattern = "(?i)WaterLevel") #Only include water level datasets in current data
+
+#load the files and save as a csv in a for loop 
+for (file in files){
+  
+  #load in the data 
+  dt <- read_datalogger_file_waterlevel(file)
+  
+  #filter out any weird time stamps that are marked before 2018
+  dt <- dt %>%
+    filter(year(TIMESTAMP) > 2018)
+  
+  #change basename file from gcrew_met site to align with previous files site name for consistency
+  if (grepl("GCREW_MET_GCREW_WaterLevel200", file)) {
+    filename <- substr(basename(file),1, (nchar(basename(file))-19))
+    filename <- gsub("GCREW_MET_GCREW_WaterLevel200", "gcrew_met_gcrewwaterlevel200", filename)
+  } else{
+    filename <- substr(basename(file),1, (nchar(basename(file))-19))
+  }
+  
+  #Make sure timestamps are stored properly
+  dt$TIMESTAMP <- ifelse(nchar(dt$TIMESTAMP) == 10, paste0(dt$TIMESTAMP, " 00:00:00"), dt$TIMESTAMP)
+  dt$TIMESTAMP <- as.character(format(as.POSIXct(dt$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S"), format = "%Y-%m-%d %H:%M:%S"))
+  
+  #write and move newly converted .csv file
+  write_monthly_data_waterlevel_01(dt, rawCSVData_dir, filename)
+  
+  #move the raw loggernet data file into the archive
+  file.rename(file, paste0(rawDataArchive_dir, basename(file)))
+  
+}
+
---
+++ WaterLevel/data_processing_scripts/03_WaterLevel_Combine.R
@@ -0,0 +1,113 @@
+#### Step 3 -- Combine Normalized Data from All Tables #### 
+#Description -- 
+#Clean the data in each table based on a low and high limit (data range)
+#Put the data in long format using the design table (normalize it)
+#ensure correct time stamp formatting 
+
+####------------------Required User Input-------------------------------------#####
+#We only want to do this on yearly files that have ALL of their loggernet data.
+#enter in the vector below the year you want to combine
+
+year <- "2025"
+
+#Load necessary packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(reshape2, lubridate, data.table, tools, plyr, tidyverse, stringr)
+
+#------------------- End Required user input --------------------------------------#
+
+#### Load Functions and Directories ####
+#Load all functions 
+invisible(lapply(list.files("functions/", pattern = "\\.R$", full.names = TRUE), source))
+
+#relevant directories
+L0_NormalizedYearly_dir <- paste0(Sys.getenv("dropbox_filepath") , "Taylor_Projects/TEST/2_L0_NormalizedData/processed/yearly/")
+L0_CombinedYearly_dir <- paste0(Sys.getenv("dropbox_filepath") , "Taylor_Projects/TEST/2_L0_NormalizedData/processed/combined/")
+#researcherdata_dir <- (paste0(Sys.getenv("TE_dropbox_filepath"), "GCREW_RESEARCHER_DATA"))
+
+  #Get files
+  i <-list.files(L0_NormalizedYearly_dir, pattern = NULL, all.files = FALSE,
+               full.names = TRUE, recursive = F,
+               ignore.case = FALSE, include.dirs = F)
+
+  # Get years from filename
+  years <- sapply(strsplit(i, split = "_"), tail, 1)
+  years <- unique(substr(years, 1, 4))
+
+#### Combine Steps -- done one yearly file at a time ####
+  #Get files with that year
+  group <- i[grepl(year, i)]
+
+  #Read in first datatable
+  dt <- fread(group[1])
+
+  #Rbind all the rest
+  if(length(group) > 1){
+    for(m in 2:length(group)){
+      dt2 <- fread(group[m])
+      dt <- rbind.fill(dt, dt2)
+    }
+  }
+
+  #Subtract offsets
+  # c3log = 45
+  # c4log = 65
+  # gcrew_met = 58
+  # genx = 76
+  # genxhydros = 56
+  dt$offset <- ifelse(dt$site == "c3", 45, 
+                      ifelse(dt$site == "c4", 65,
+                             ifelse(dt$site == "gcrew_met", 58,
+                                    ifelse(dt$site == "genx", 76, 
+                                           ifelse(dt$site == "genxhydros", 56,
+                                                  ifelse(dt$site == "c3_floatgauge", 45, NA))))))
+  
+  #Add corrected depth column
+  dt$corrected_depth <- dt$depth - dt$offset
+  
+  #Subset to only useful columns
+  keep <- c("site", "time2", "offset", "depth", "corrected_depth", "actual_conductivity", 
+            "specific_conductivity", "pressure",  
+            "resistivity", "salinity", "tds", "temperature", "water_density")
+  
+  dt2 <- subset(dt, select = keep)
+  setDT(dt2)
+  
+  #Order dt2 by time2
+  dt2 <- dt2[order(time2)]
+  
+  #NOW THE DATA IS IN LONG FORM. THIS IS NICE....but Roy wants it in wide form
+  #Make it even longer
+  dt_long <- melt(dt2, id.vars = c("time2", "site"), variable.name = "colnames", value.name = "value", na.rm = T)
+  dt_long$newname <- paste0(dt_long$colnames, ".", dt_long$site)
+  
+  dt_long <- dt_long[,c(1, 4, 5)]
+  
+  dt_long <- unique(dt_long)
+
+  #Now make it wide
+  dt_wide <- dcast(dt_long, time2 ~ newname, subset = NULL, drop = TRUE, value.var = "value")
+  
+  #Save wide format
+  filename <- paste0("waterlevel_combined_WIDE_", year, ".csv")
+  out_path <- file.path(L0_CombinedYearly_dir, filename)
+  
+  #Format timestamp nicely for midnight tzs
+  dt_wide$time2 <- format(as.character(dt_wide$time2))
+
+  write.table(dt_wide, out_path, append = FALSE, quote = FALSE, sep = ",",
+              na = "NA", dec = ".", row.names = FALSE,
+              col.names = TRUE, qmethod = c("escape", "double"))
+  
+  #Save long format
+  filename <- paste0("waterlevel_combined_TRUELONG_", year, ".csv")
+  out_path <- file.path(L0_CombinedYearly_dir, filename)
+  
+  #Format timestamp nicely for midnight tzs
+  dt_long$time2 <- format(as.character(dt2$time2))
+
+  write.table(dt_long, out_path, append = FALSE, quote = FALSE, sep = ",",
+              na = "NA", dec = ".", row.names = FALSE,
+              col.names = TRUE, qmethod = c("escape", "double"))
+
+  
